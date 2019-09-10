8d244d6dee5191f9afd2b94fd7e15df027582495
mdeejay <mdjrussia@gmail.com>
2019-09-10 09:47:39 +0200

    Adapt for Q
    Signed-off-by: mdeejay <mdjrussia@gmail.com>

--- Android.mk
+++ Android.mk
@@ -1,7 +1,6 @@
-ifeq ($(AB_OTA_UPDATER),true)
+ifeq ($(strip $(TARGET_USES_HARDWARE_QCOM_BOOTCTRL)),true)
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(BOARD_IS_AUTOMOTIVE),true)
-ifneq ($(filter msm8953 msm8996 msm8998 sdm660 sdm845 sdm710 trinket,$(TARGET_BOARD_PLATFORM)),)
 LOCAL_PATH := $(call my-dir)
 
 # HAL Shared library for the target. Used by libhardware.
@@ -12,7 +11,7 @@ LOCAL_SHARED_LIBRARIES += liblog libgptutils libcutils
 LOCAL_HEADER_LIBRARIES := libhardware_headers libsystem_headers
 LOCAL_SRC_FILES := boot_control.cpp
 LOCAL_MODULE_RELATIVE_PATH := hw
-LOCAL_MODULE := bootctrl.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := bootctrl.$(PRODUCT_BOARD_PLATFORM)
 LOCAL_MODULE_OWNER := qcom
 LOCAL_PROPRIETARY_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
@@ -24,9 +23,8 @@ LOCAL_CFLAGS += -Wall -Werror
 LOCAL_SHARED_LIBRARIES += liblog libgptutils libcutils
 LOCAL_HEADER_LIBRARIES := libhardware_headers libsystem_headers
 LOCAL_SRC_FILES := boot_control.cpp
-LOCAL_MODULE := bootctrl.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE := bootctrl.$(PRODUCT_BOARD_PLATFORM)
 include $(BUILD_STATIC_LIBRARY)
 
-endif
 endif
 endif
